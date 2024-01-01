edbfe7cb9f82aff473e88afe8c2a4e41b370559b
Omar <omarabbasi16@gmail.com>
2023-12-31 19:19:54 -0500

    implemented pool class for registry

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -55,8 +55,67 @@ class System{
 
 };
 
+
+//Pool
+//A pool is just a vector of objects of type T
+class IPool{
+    public:
+        virtual ~IPool(){}
+};
+template <typename T>
+class Pool: public IPool{
+    private:
+        std::vector<T> data;
+    public:
+        Pool(int size = 100){
+            data.resize(size);
+        }
+        virtual ~Pool() = default;
+        
+        bool isEmpty() const{
+            return data.empty();
+        }
+
+        int getSize() const{
+            return data.size();
+        }
+
+        void Resize(int n){
+            data.resize(n);
+        }
+
+        void Clear(){
+            data.clear();
+        }
+
+        void Add(T object){
+            data.push_back(object);
+        }
+
+        void Set(int index, T object){
+            data[index] = object;
+        }
+
+        T& Get(int index){
+            return static_cast<T&>(data[index]);
+        }
+
+        T& operator [](unsigned int index){
+            return data[index];
+        }
+};
+
+//Registry
+//The registry manages the creation and destruction of entities, systems, and components.
 class Registry{
-    //TODO: ...
+    private:
+        int numEntities = 0;
+                
+        //Vector of component pools
+        //Each pool contains all the data for a certain component type
+        //Vector index = component type id
+        //Pool index - entity id
+        std::vector<IPool*> componentPools;
 };
 
 template <typename TComponent>
