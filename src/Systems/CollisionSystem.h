ab7960dbda509bbafe3aaa71f538bb484e5fe407
Omar <omarabbasi16@gmail.com>
2024-01-09 09:40:45 -0500

    refactored collision system using SDL rect

--- src/Systems/CollisionSystem.h
+++ src/Systems/CollisionSystem.h
@@ -1,18 +1,12 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+
 #include <unordered_map>
 #include "../ECS/ECS.h"
 #include "../Components/BoxColliderComponent.h"
 #include "../Components/TransformComponent.h"
 #include "../Logger/Logger.h"
-
-// Struct holding the required data for calculating collisions
-struct BoundingBox{
-    int x;
-    int y;
-    int width;
-    int height;
-};
 
 class CollisionSystem: public System{
     public:
@@ -22,8 +16,8 @@ class CollisionSystem: public System{
         }
 
         void Update(){
-            // Create a map: key = entityId, value = BoundingBox 
-            unordered_map<int, BoundingBox> entityBoxes; 
+            // Create a map to cache bounding boxes: key = entityId (int), value = bounding box (SDL_Rect)  
+            unordered_map<int, SDL_Rect> entityBoxes; 
             
             // Loop over system entities
             for (auto entity: GetSystemEntities()){
@@ -31,24 +25,22 @@ class CollisionSystem: public System{
                 const auto transform = entity.GetComponent<TransformComponent>();
                 
                 // Create a bounding box for the current entity
-                BoundingBox box;
-                box.x = transform.position.x;
-                box.y = transform.position.y;
-                box.width = transform.position.x + collider.width;
-                box.height = transform.position.y + collider.height;
+                SDL_Rect boxA {
+                    (int)transform.position.x,
+                    (int)transform.position.y,
+                    collider.width,
+                    collider.height
+                };
                 
                 // Loop over the map - check for collisions and log 
-                for(const std::pair<const int, BoundingBox>& mapBox : entityBoxes){
-                    if(
-                        box.x < mapBox.second.width &&
-                        box.width > mapBox.second.x &&
-                        box.y < mapBox.second.height &&
-                        box.height > mapBox.second.y
-                    ) Logger::Log("Collision between entity " + to_string(entity.GetId()) + " and entity " + to_string(mapBox.first));
+                for(const std::pair<const int, SDL_Rect>& boxB : entityBoxes){
+                    if(SDL_HasIntersection(&boxA, &boxB.second)){
+                        Logger::Log("Collision between entity " + to_string(entity.GetId()) + " and entity " + to_string(boxB.first));
+                    } 
                 }
                 
                 // Push the key/value pair of current entity and current bounding box into the map
-                entityBoxes[entity.GetId()] = box; 
+                entityBoxes.insert({entity.GetId(), boxA}); 
             }
         }
 };
