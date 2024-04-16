175192e4a82d2207333379534609efc0ab43ba90
Omar <omarabbasi16@gmail.com>
2024-04-16 09:24:23 -0400

    testing lua integration via sol

---
+++ assets/scripts/myscript.lua
@@ -0,0 +1,8 @@
+-- Lua global variable
+test_variable = 5 * 2
+user_name = "test mctesterson"
+
+print ("Hello "..user_name)
+if test_variable == 10 then
+    print("The value of the test variable is "..test_variable)
+end
